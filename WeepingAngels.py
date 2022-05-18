2d851322b694e6f5e8937c42b64b2144c06d8e2b
DucksIncoming <ducksincoming@gmail.com>
2022-05-17 20:20:08 -0400

    Spell UI Visuals

--- WeepingAngels.py
+++ WeepingAngels.py
@@ -1,26 +1,71 @@
-# Weeping Angels v0.0
+# Weeping Angels v1.0
 # by DucksIncoming
-# 1/21/22
+# 5/17/22
 # https://github.com/DucksIncoming/weeping-angels
 
-lastFace = 0 # Only need the X component.
+# Thank god cv2 exists this literally took 10 minutes to implement lmaooo
+import cv2
+import time
+from pyfirmata import Arduino, SERVO
 
-def stopMovement():
-  # If the statue is moving, stop it in place.
-  # If not moving, this does nothing.
+# Arduino config
+port = "COM6" # Set to the USB port of your arduino device
+servoPin = 9 # Set to your servo pin (digital only)
+debugMode = True
+NoFaceFound = True
 
-  return true
+if (not debugMode):
+  try:
+    board = Arduino(port)
+  except:
+    print("Arduino board not plugged in! (Or not accessible on specified port)")
+    time.sleep(5000)
+    quit()
 
-def lookForFace():
-  # Code for facial detection
+  board.digital[servoPin].mode = SERVO
+  board.digital[servoPin].write(0)
 
-  # Returns true if face found and sets lastFace,
-  # returns false if no face found
+capture = cv2.VideoCapture(0)
+lastFace = [0, 0]
+TimeSinceLastFace = 0
+rotationMemory = 0.0
+targetRotation = 0.0
 
-  lastFace = 100
-  return true
+while True:
+  _, img = capture.read()
+  face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  faces = face_cascade.detectMultiScale(gray, 1.1, 8)
 
-if (lookForFace()):
-  stopMovement()
-else:
-  # Rotate head until aligned with lastFace
+  targetRotation = ((lastFace[0] - 160.0) / 160.0) * 30.0
+
+  # This is sketchy but im bored lol
+  i = 0
+  for (x, y, w, h) in faces:
+    if (i == 0):
+      NoFaceFound = False
+      TimeSinceLastFace = 0
+      faceFound = True
+      cv2.rectangle(img, (x, y), (x+w, y+h), (255,0,0), 2)
+      i += 1
+      lastFace = [(x + w) / 2, (y + h) / 2]
+
+  cv2.imshow('Weeping Angels GUI', img)
+  TimeSinceLastFace += 1
+
+  print(TimeSinceLastFace)
+
+  if (TimeSinceLastFace > 15 and NoFaceFound == False):
+    print(lastFace)
+    if (not debugMode):
+      board.digital[servoPin].write(rotationMemory + targetRotation)
+    rotationMemory += targetRotation
+    print(targetRotation)
+    NoFaceFound = True
+    TimeSinceLastFace = 0
+  
+  k = (cv2.waitKey(30) & 0xff)
+  if (k==27):
+    break
+
+capture.release()
